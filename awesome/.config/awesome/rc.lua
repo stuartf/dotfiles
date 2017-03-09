cb667d436b673a79a82ec2eee0e6faff999ba2dc
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2017-03-09 08:55:04 -0500

    put the hangouts app on tag 2

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -469,6 +469,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+
+    -- Hangouts on tag 2
+    { rule = { instance = "crx_knipolnnllmklapflnccelgolnpehhpl" },
+      properties = { screen = 1, tag = "2" } },
 }
 -- }}}
 
